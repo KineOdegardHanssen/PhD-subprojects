ebcaee4e70175632787d47384f4a6fe0078e79ac
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2020-01-02 15:36:45 +0100

    Bead diffusion: Averages
    Implemented averages in analysis of bead-in-brush and bulk bead simulations. Also included a conversion to SI units, which yielded a more sensible constant term in the line fit. Made some additional plots. Will go on to only look at sections of the data in order to find an appropriate diffusion constant. That might be hard.

--- MD_analysis/diffusion_bulk.py
+++ MD_analysis/diffusion_bulk.py
@@ -26,14 +26,14 @@ def rmsd(x,y):
     return delta
 
 # Input parameters for file selection: # I will probably add more, but I want to make sure the program is running first
-spacing = 5
+spacing = 5 #100
 
 # I need to set the file name in an easier way, but for now I just use this:  ## Might want to add a loop too, if I have more files...
 
 # Should divide into folders in a more thorough manner?
-# Extracting the correct names (all of them
-plotseed = 0
+# Extracting the correct names (all of them)
 plotdirs = False
+startpart = '_'#'_withsubstrate_'
 test_sectioned = True
 seeds  = [23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113]
 Nseeds = len(seeds)
@@ -42,16 +42,16 @@ unitlength   = 1e-9
 unittime     = 2.38e-11 # s
 timestepsize = 0.00045*unittime
 print('timestepsize:', timestepsize)
-Npartitions = 5 # For extracting more walks from one file (but is it really such a random walk here...?)
-namebase    = '_quadr_M9N101_ljunits_spacing%i_Langevin_Kangle14.0186574854529_Kbond140.186574854529_debye_kappa1_debcutoff3_chargeel-1_effdiel0.00881819074717447_T3_theta0is180_pmass1.5_sect_placeexact_ljcut1p122' %spacing
-folderbase  = 'Part_in_chgr_subst_all_quadr_M9N101_ljunits_Langevin_Kangle14.0186574854529_Kbond140.186574854529_debye_kappa1_debcutoff3_chargeel-1_effdiel0.00881819074717447_T3_theta0is180_pmass1.5_sect_placeexact_ljcut1p122'
+Npartitions  = 5 # For extracting more walks from one file (but is it really such a random walk here...?)
+namebase = 'bulkdiffusion'+startpart+'ljunits_spacing%i_Langevin_T3_pmass1.5_sect_placeexact_ljepsilon0.730372054992096_ljcut1p122' % spacing
+folderbase = 'Bulkdiffusion'+startpart+'ljunits_Langevin_T3_pmass1.5_sect_placeexact_ljepsilon0.730372054992096_ljcut1p122'
+namebase_short = namebase # In case I ever need to shorten it
 endlocation       = '/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Diffusion_bead_near_grid/'+folderbase+'/Spacing'+str(spacing)+'/'
-outfilename       = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'.txt'
-outfilename_gamma = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_zimportance'+'.txt'
-plotname          = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'.png'
-plotname_all      = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_all.png'
-plotname_gamma    = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_zimportance'+'.png'
-plotname_SI       = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_SI.png'
+outfilename       = endlocation+'lammpsdiffusion_'+namebase_short+'.txt'
+outfilename_gamma = endlocation+'lammpsdiffusion_'+namebase_short+'_zimportance'+'.txt'
+plotname          = endlocation+'lammpsdiffusion_'+namebase_short+'.png'
+plotname_gamma    = endlocation+'lammpsdiffusion_'+namebase_short+'_zimportance'+'.png'
+plotname_SI       = endlocation+'lammpsdiffusion_'+namebase_short+'_SI'+'.png'
 # Setting arrays
 # These are all squared:
 # All together:
@@ -78,68 +78,17 @@ rmsds         = []
 Nins          = []
 # This is not squared, obviously:
 alltimes  = []
+#times_single = np.zeros(Nsteps) # I set this in the loop, after having extracted dt
 
 for seed in seeds:
     print('On seed', seed)
     seedstr = str(seed)
-    infilename_all  = 'part_in_chgr_subst_all'+namebase+'_seed'+seedstr+'.lammpstrj'
-    infilename_free = 'part_in_chgr_subst_freeatom'+namebase+'_seed'+seedstr+'.lammpstrj'
-    plotname_dirs   = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_dxdydzR2_seed'+seedstr+'.png'
-    plotname_testsect = endlocation+'lammpsdiffusion_qdrgr_'+namebase+'_testsectioned_seed'+seedstr+'.png'
-    
+    infilename_free = namebase+'_seed'+seedstr+'.lammpstrj'
+    plotname_dirs   = endlocation+'lammpsdiffusion_'+namebase_short+'_dxdydzR2_seed'+seedstr+'.png'
+    plotname_testsect = endlocation+'lammpsdiffusion_'+namebase_short+'_testsectioned_seed'+seedstr+'.png'
+    #print('infilename:',infilename_free)
     # Read in:
     #### Automatic part
-    ## Find the extent of the polymers: Max z-coord of beads in the chains
-    infile_all = open(infilename_all, "r")
-    lines = infile_all.readlines() # This takes some time
-    # Getting the number of lines, etc.
-    totlines = len(lines)         # Total number of lines
-    lineend = totlines-1          # Index of last element
-    
-    # Extracting the number of atoms:
-    words = lines[3].split()
-    Nall = int(words[0])
-    N    = Nall
-    
-    skiplines   = 9             # If we hit 'ITEM:', skip this many steps...
-    skipelem    = 0
-    sampleevery = 0
-    i           = int(math.ceil(skipelem*(Nall+9)))
-    skiplines  += (Nall+skiplines)*sampleevery # Check!
-    
-    # For now: Only find the largest z-position among the beads in the chain. # Otherwise I must read off the atom number (will do that with the free bead anyways.)
-    maxz = -1000 # No z-position is this small.
-    
-    time_start = time.process_time()
-    counter = 0
-    while i<totlines:
-        words = lines[i].split()
-        if (words[0]=='ITEM:' and words[1]=='TIMESTEP'): # Some double testing going on...
-            if words[1]=='TIMESTEP':
-                i+=skiplines
-            elif words[1]=='NUMBER': # These will never kick in. 
-                i+=7
-            elif words[1]=='BOX':
-                i+=5
-            elif words[1]=='ATOMS':
-                i+=1
-        elif len(words)<8:
-            i+=1
-        else:
-            # Find properties
-            # Order:  id  type mol ux  uy  uz  vx  vy   vz
-            #         [0] [1]  [2] [3] [4] [5] [6] [7]  [8]
-            ind      = int(words[0])-1 # Atom ids go from zero to N-1.
-            atomtype = int(words[1]) 
-            #molID    = int(words[2])
-            z        = float(words[5])
-            if atomtype==2: # Moving polymer bead. Test if this is larger than maxz:
-                if z>maxz:
-                    maxz = z
-            i+=1
-    extent_polymers = maxz
-    infile_all.close()
-    
     ## Find the position of the free bead: # I reuse quite a bit of code here...
     infile_free = open(infilename_free, "r")
     lines = infile_free.readlines() # This takes some time
@@ -205,9 +154,8 @@ for seed in seeds:
     
     time_end = time.process_time()
     
-    pos_inpolymer = []
+    positions = []
     
-    #before_in
     
     # I do not take into account that the free bead can enter the polymer grid and then exit again. If that happens, there might be discontinuities or weird kinks in the data (if you look at the graphs...) # I do not have this in my test-dataset. Maybe make the bead lighter and see what happens?
     
@@ -216,15 +164,12 @@ for seed in seeds:
     for i in range(counter):
         thesepos = freeatom_positions[i]
         z        = thesepos[2]
-        if z>extent_polymers: # If the polymer is in bulk # We don't want it to go back and forth between brush and bulk # That will cause discontinuities in our data
-            break
-        else:
-            pos_inpolymer.append(thesepos)
-            if z>maxzpol:
-                maxzpol = z
-            Nin+=1
+        positions.append(thesepos)
+        if z>maxzpol:
+            maxzpol = z
+        Nin+=1
     
-    startpos_in   = pos_inpolymer[0]
+    startpos_in   = positions[0]
         
     #######
     # Will divide into several walks with different starting points later on
@@ -249,7 +194,7 @@ for seed in seeds:
     allRs.append(0)        # We will set this here since we know the value
     alltimes.append(0)
     for i in range(1,Nin):       
-        this_in = pos_inpolymer[i]
+        this_in = positions[i]
         dist = this_in-startpos_in
         R2   = np.dot(dist,dist)
         dx2  = dist[0]*dist[0]
@@ -304,7 +249,7 @@ for seed in seeds:
         plt.plot(step_temp, dz_temp, label=r'$<dz^2>$')
         plt.xlabel(r'Step number')
         plt.ylabel(r'Distance$^2$ [in unit length]')
-        plt.title('Random walk in polymer brush, seed %s, d = %i nm' % (seedstr,spacing))
+        plt.title('RMSD in bulk, seed %s, d = %i nm' % (seedstr,spacing))
         plt.tight_layout()
         plt.legend(loc='upper left')
         plt.savefig(plotname_dirs)
@@ -321,10 +266,10 @@ for seed in seeds:
         part_start   = i*smallest_part_walk
         len_thiswalk = Nin-part_start       # Do I need this?
         #print('part_start:', part_start)
-        #print('len(pos_inpolymer):', len(pos_inpolymer))
-        rstart = pos_inpolymer[part_start]
+        #print('len(positions):', len(positions))
+        rstart = positions[part_start]
         for j in range(len_thiswalk):
-            rthis =  pos_inpolymer[part_start+j]
+            rthis = positions[part_start+j]
             drvec = rthis - rstart
             dr2   = np.dot(drvec,drvec)
             this_part.append(dr2)
@@ -338,7 +283,7 @@ for seed in seeds:
             plt.plot(part_steps[i], part_walks[i], label='Section %i' % i)
         plt.xlabel(r'Step number')
         plt.ylabel(r'Distance$^2$ [in unit length]')
-        plt.title('Random walk in polymer brush, seed %s, d = %i nm' % (seedstr,spacing))
+        plt.title('RMSD in bulk, seed %s, d = %i nm' % (seedstr,spacing))
         plt.tight_layout()
         plt.legend(loc='upper left')
         plt.savefig(plotname_testsect)
@@ -362,28 +307,15 @@ Nins_sorted          = np.zeros(Nseeds)
 single_slopes_sorted = np.zeros(Nseeds)
 
 # Calculating averages:
-Ninbrush = Nsteps # Default, in case it does not exit the brush
-for i in range(1,Nsteps):
+
+for i in range(Nsteps):
     counter = average_counter[i]
     if counter!=0:
         averageRs[i]/=counter
         averagedxs[i]/=counter
         averagedys[i]/=counter
         averagedzs[i]/=counter
-    else:
-       Ninbrush = i-1
-       break
 
-times_single = times_single[0:Ninbrush]
-times_single_real = times_single_real[0:Ninbrush]
-averageRs  = averageRs[0:Ninbrush]
-averagedxs = averagedxs[0:Ninbrush]
-averagedys = averagedys[0:Ninbrush]
-averagedzs = averagedzs[0:Ninbrush]
-
-print('len(averageRs):',len(averageRs))
-print('Ninbrush:',Ninbrush)
-print('Nsteps:', Nsteps)
 
 # Opening file
 outfile_gamma = open(outfilename_gamma, 'w')
@@ -455,22 +387,11 @@ print('Fit, average, SI:')
 print('b_poly (should be small):', b_poly_SI)
 print('D_poly:', D_poly_SI)
 
-
 outfile = open(outfilename, 'w')
 outfile.write('D_poly: %.16f %.16f\n' % (D_poly,rms_D_poly))
 outfile.write('b_poly: %.16f %.16f\n' % (b_poly,rms_b_poly))
 outfile.close()
     
-plt.figure(figsize=(6,5))
-plt.plot(alltimes, allRs, ',', label='Data, brush')
-plt.plot(times_single, fit_poly_av, '--', label='Fit, brush')
-plt.xlabel(r'Step number')
-plt.ylabel(r'Distance$^2$ [in unit length]')
-plt.title('RMSD in bulk, d = %i nm' % spacing)
-plt.tight_layout()
-plt.legend(loc='upper left')
-plt.savefig(plotname)
-
 plt.figure(figsize=(6,5))
 plt.plot(alltimes, allRs, ',', label='Data, brush')
 plt.plot(times_single, averageRs, ',', label='Average, brush')
@@ -481,13 +402,13 @@ plt.ylabel(r'Distance$^2$ [in unit length]')
 plt.title('RMSD in bulk, d = %i nm' % spacing)
 plt.tight_layout()
 plt.legend(loc='upper left')
-plt.savefig(plotname_all)
+plt.savefig(plotname)
 
 plt.figure(figsize=(6,5))
 plt.plot(plotgammaagainst, sorted_gamma_avgs)
 plt.xlabel(r'Different runs')
 plt.ylabel(r'$\gamma = <\frac{R^2-dz^2}{R^2}>$')
-plt.title(r'Random walk in polymer brush, $\gamma$; d = %i nm' % spacing)
+plt.title(r'RMSD in bulk, $\gamma$; d = %i nm' % spacing)
 plt.tight_layout()
 plt.savefig(plotname_gamma)
 
@@ -501,4 +422,3 @@ plt.tight_layout()
 plt.legend(loc='upper left')
 plt.savefig(plotname_SI)
 
-print('counters:',average_counter)
