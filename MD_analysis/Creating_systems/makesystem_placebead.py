11a0117798a723ee3c2b9850ca9f23263dee8d1c
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2020-01-10 16:59:23 +0100

    Working more on placing the bead
    I thought I could copy almost all the lines and just add one. A new issue arised, though: I need to update the number of atoms. I also need to add a line in the Velocities-section. Maybe something else will show up too.

--- MD_analysis/Creating_systems/makesystem_placebead.py
+++ MD_analysis/Creating_systems/makesystem_placebead.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy as np
 import random
 import sys
 import math
@@ -14,6 +14,14 @@ charge  = -1
 Nfiles = 21 # The number of files I had before
 seed   = 23 # Or somethin' 
 
+# Technicalities: Placement of bead
+tol        = 0.2 # The closest we'll allow the bead to be to a brush 'atom'
+substr_tol = tol # the closest we'll allow the bead to be to the substrate
+distrange  = 1   # The spread of z-values of the bead. E.g. from 0.5 to 1.5. Will be from substr_tol to substr_tol+distrange
+
+# 
+Ncopylines = 36 # The number of lines we are going to read from the data.-infile and write to the data.-outfile # I will update this with the number of atoms later, so it should be sort of automatic
+
 # Could I just write chunks of text to file without touching it? Use line in lines to write? I only really need the atom positions now.
 
 brush_beads = [] # We know where the substrate atoms are (at z=0), but we should know where the beads are so that we can avoid overlap
@@ -23,25 +31,42 @@ brush_beads = [] # We know where the substrate atoms are (at z=0), but we should
 LAMMPS data file via write_data, version 11 Aug 2017, timestep = 200000 
 
 # Line 2 to 7 (enumeration starting at 0)
-1134 atoms
+1134 atoms # And I'll add one... Can't just copy this line...
 4 atom types
 900 bonds
 1 bond types
 891 angles
 1 angle types
+
+# Line 9 to 11 (in 0-enumeration)
+-2.5000000000000000e+00 1.2500000000000000e+01 xlo xhi
+-2.5000000000000000e+00 1.2500000000000000e+01 ylo yhi
+0.0000000000000000e+00 3.0000000000000000e+02 zlo zhi
+
 '''
 
 infilename = 'data.chaingrids_substrate_N909_Nchains9_Ly3_gridspacing%i_twofixed_charge%i_mass1_equilibrated' % (spacing, charge)
 infile     = open(infilename, 'r')
 lines      = infile.readlines()
+Nlines     = len(lines)            # I will probably need this to write as many lines to file as necessary
+infile.close()
 
-N_atoms      = float(lines[2].split()[0]) # Line 2
-N_atomtypes  = float(lines[3].split()[0]) # Line 3
-N_bonds      = float(lines[4].split()[0]) # Line 4
-N_bondtypes  = float(lines[5].split()[0]) # Line 5
-N_angles     = float(lines[6].split()[0]) # Line 6
-N_angletypes = float(lines[7].split()[0]) # Line 7
+N_atoms      = int(lines[2].split()[0]) # Line 2
+N_atomtypes  = int(lines[3].split()[0]) # Line 3
+N_bonds      = int(lines[4].split()[0]) # Line 4
+N_bondtypes  = int(lines[5].split()[0]) # Line 5
+N_angles     = int(lines[6].split()[0]) # Line 6
+N_angletypes = int(lines[7].split()[0]) # Line 7
 
+
+xmin = float(lines[9].split()[0]) 
+xmax = float(lines[9].split()[1])
+ymin = float(lines[10].split()[0])
+ymax = float(lines[10].split()[1])
+zmin = float(lines[9].split()[0])
+zmax = float(lines[9].split()[1])
+
+Ncopylines += N_atoms
 
 #atom_nr   = []
 molID     = np.zeros(N_atoms)
@@ -66,7 +91,7 @@ for line in lines:
     if words[0]=='Atoms': # Do I need these?
         keyword = 'Atoms'
     '''
-    if words[0]=='Velocities':
+    if len(words)>0 and words[0]=='Velocities':
         break # I'll do this if I can write read lines directly to file.
         #keyword = 'Velocities'
     '''
@@ -77,16 +102,19 @@ for line in lines:
     '''
     if len(words)==10:
         i = int(words[0])
-        this_atom_type = int(words[2])
         molID[i-1]     = int(words[1])
+        this_atom_type = int(words[2])
+        xposition      = float(words[4])
+        yposition      = float(words[5])
+        zposition      = float(words[6])
         atom_type[i-1] = this_atom_type
-        qs[i-1]        = int(words[3])
-        xpos[i-1]      = int(words[4])
-        ypos[i-1]      = int(words[5])
-        zpos[i-1]      = int(words[6])
+        qs[i-1]        = float(words[3])
+        xpos[i-1]      = xposition
+        ypos[i-1]      = yposition
+        zpos[i-1]      = zposition
         # Check if this is a brush atom, then append it to the brush list if it is.
         if this_atom_type==1 or this_atom_type==2:
-            brush_beads.append(i-1)
+            brush_beads.append(np.array([xposition,yposition,zposition]))
     '''
     if len(words)==4:
         if keyword=='Velocities':
@@ -102,9 +130,35 @@ for line in lines:
 
 ### Part where I randomly place the diffusing bead.
 
-for i in range(Nfiles): # I need to get the box edges here
+beadpos = []
+
+i = 0
+while i<Nfiles: # I need to get the box edges here # USE CONTINUE??????
+    breakit = False
     xran = random.uniform(xmin,xmax)
     yran = random.uniform(ymin,ymax)
-    zran = random.uniform(1,3) # Random placement not too far from the substrate.
+    zran = random.uniform(substr_tol,substr_tol+distrange) # Random placement not too far from the substrate.
+    rran = np.array([xran,yran,zran])
+    for rbr in brush_beads:
+        distvec = rbr-rran
+        dist2   = np.dot(distvec,distvec)
+        if dist2<tol:
+            breakit = True
+            break
+    if breakit==True:
+        continue
+    else:
+        beadpos.append(rran)
+        i+=1
 
+print('beadpos:',beadpos)
 
+############# Write to file. Multiple times. #############
+for i in range(Nfiles):
+    filename = infilename + '_bead_file%i' % (i+1)
+    outfile = open(filename, 'w')
+    # Write lines from infile to the outfile.
+    for j in range(Ncopylines):
+        # if-test to update number of atoms?
+        outfile.write(lines[j])
+    outfile_write('%i %i 3' % (, max(molID)+1, )) # atom-ID molecule-ID atom-type q x y z
