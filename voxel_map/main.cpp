2fe7bf138b045c056a625b1ab09838dac0afb75b
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2020-02-27 14:46:31 +0100

    Program runs and results seem fair
    It looks like I found all the bugs. I think perhaps the values of the voxel matrix seemed too similar? At least in the xy-planes, they were almost the same. But I can't spot any bugs. Should test the results more thoroughly.

--- voxel_map/main.cpp
+++ voxel_map/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 using std::ofstream; using std::string;
 
 vector<int> int_linspace(int framefirst, int framelast, int Nframes);
-vector<vector<vector<double>>> voxellate_basic(int frame, int Nx, int Ny, int Nz, vector<double> x_centres, vector<double> y_centres, vector<double> z_centres, vector<vector<double>> xes, vector<vector<double>> ys, vector<vector<double>> zs); // voxellate(Nx, Ny, Nz, x_centres, y_centres, z_centres, posvecs)
+vector<vector<vector<double>>> voxellate_basic(int frame, int Nall, int Nx, int Ny, int Nz, vector<double> x_centres, vector<double> y_centres, vector<double> z_centres, vector<vector<double>> xes, vector<vector<double>> ys, vector<vector<double>> zs); // voxellate(Nx, Ny, Nz, x_centres, y_centres, z_centres, posvecs)
 
 //vector<vector<double>> voxellate_basic(int Nx, int Ny, int Nz, vector<double> x_centres, vector<double> y_centres, vector<double> z_centres, vector<vector<double>> xes, vector<vector<double>> ys, vector<vector<double>> zs); // voxellate(Nx, Ny, Nz, x_centres, y_centres, z_centres, posvecs)
 
@@ -22,7 +22,7 @@ int main()
 {
     int M, N, gridspacing;
     double wall, charge, T, Kangle, Kbond;
-    string filename;
+    string infilenamePrefix, outfilenamePrefix; //filename, outfilename, xfilename, yfilename, zfilename, datafilename;
 
     int Nvoxels    = 55;
     int totframes  = 10000000/10000;
@@ -34,12 +34,16 @@ int main()
     //filename = "C:/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/chaingrid_quadratic_M9N101_gridspacing40_Langevin_wall1.042_Kangle20_Kbond200_debye_kappa1_debyecutoff3_charge-1_T3_theta0is180_twofirst_are_fixed.lammpstrj";
 
     // File name for testing (look more into which files I've deleted and which I haven't later): # And look into paths.
+    infilenamePrefix  = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/chaingrid_quadratic_M9N101_ljdebye1.042_angle_Langevin_wall1.042_Kangle25_Kbond2000_T310_theta0is180_cutoffs3_sigma1_firstatomfixed";
+    outfilenamePrefix = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/chaingrid_quadratic_M9N101_ljdebye1.042_angle_Langevin_wall1.042_Kangle25_Kbond2000_T310_theta0is180_cutoffs3_sigma1_firstatomfixed";
+    /*
     filename = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/chaingrid_quadratic_M9N101_ljdebye1.042_angle_Langevin_wall1.042_Kangle25_Kbond2000_T310_theta0is180_cutoffs3_sigma1_firstatomfixed.lammpstrj";
     outfilename = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/chaingrid_quadratic_M9N101_ljdebye1.042_angle_Langevin_wall1.042_Kangle25_Kbond2000_T310_theta0is180_cutoffs3_sigma1_firstatomfixed_step%i.txt";
     xfilename   = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/x_centres.txt";   // Strictly speaking, I don't need these, but they do make things a bit easier...
     yfilename   = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/y_centres.txt";
     zfilename   = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/z_centres.txt";
     datafilename = "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/data.txt";       // This is basically all the information I need to reconstruct the data // But easier if I write bincenters to file.
+    */
 
     M = 9;
     N = 101;
@@ -63,6 +67,8 @@ int main()
 
     ///*
     ifstream inFile;
+    char *filename = new char[100000]; // Possibly a long file name
+    sprintf(filename, "%s.lammpstrj", infilenamePrefix.c_str());
     inFile.open(filename);
     if (!inFile)
     {
@@ -126,7 +132,6 @@ int main()
             storeframe = outframes[framecount]; // Test if this is the frame we want
         }
 
-
         // For finding the bounds of the voxel matrix // Should I have this here? Takes more time, but is more robust this way.
         if (z>maxzs[mol-1]){maxzs[mol-1]=z;} // Getting the max value of z for each chain (regardless of the time step. We want the same cutting for all times) -- If we put this below the if-test, this complicates running this file several times for different output times
         if (x>maxx){maxx=x;}
@@ -156,8 +161,8 @@ int main()
     int Nx;
     double Lx, Ly, Lz, lenvoxel;
     vector<double> x_centres = vector<double>(Nvoxels);
-    vector<double> y_centres_temp = vector<double>(Nvoxels);
-    vector<double> z_centres_temp = vector<double>(Nvoxels);
+    vector<double> y_centres = vector<double>(Nvoxels);
+    vector<double> z_centres = vector<double>(Nvoxels);
 
     Nx = Nvoxels;
     Lx = maxx-minx;
@@ -170,18 +175,20 @@ int main()
     //x_centres = 0.5*(2*ns+1)*lenvoxel+minx;
     for(int i=0; i<Nvoxels; i++){
         x_centres[i] = 0.5*(2.0*ns[i]+1.0)*lenvoxel+minx;
+        //cout << "x_centres[" << i << "]:" << x_centres[i] << endl;
     }
 
-    int centrevalue, edgevalue;
+    float centrevalue, edgevalue;
     int Ny      = 0;
     int Nz      = 0;
     int edgehit = 0;
     int counter = 0;
-    double i     = 0;
+    double i    = 0;
     while (edgehit==0){
         centrevalue = 0.5*(2.0*i+1.0)*lenvoxel+miny;
         edgevalue   = lenvoxel*(i+1.0)+miny;
-        y_centres_temp[counter] = centrevalue;
+        y_centres[counter] = centrevalue;
+        //cout << "y_centres[" << counter << "]:" << y_centres[counter] << "= " << centrevalue << endl;
         i += 1.0;
         counter++;
         if ((maxy-centrevalue)<0.5*lenvoxel or abs(maxy-edgevalue)<0.1*lenvoxel){
@@ -195,7 +202,7 @@ int main()
     while (edgehit==0){
         centrevalue = 0.5*(2.0*i+1.0)*lenvoxel+minz;
         edgevalue   = lenvoxel*(i+1.0)+minz;
-        z_centres_temp[counter] = centrevalue;
+        z_centres[counter] = centrevalue;
         i += 1.0;
         counter++;
         if ((maxz-centrevalue)<0.5*lenvoxel or abs(maxz-edgevalue)<0.1*lenvoxel){
@@ -209,10 +216,8 @@ int main()
     vector<double> y_centres = vector<double>(Ny);
     vector<double> z_centres = vector<double>(Nz);
 
-    //y_centres = y_centres_temp[0:Ny];
-    //z_centres = z_centres_temp[0:Nz];
 
-    for(int i=0; i<Ny; i++){y_centres[i]=y_centres_temp[i]}
+    for(int i=0; i<Ny; i++){y_centres[i]=y_centres[i]}
     */
 
     /* // This is the Python code for the base implementation of the voxellation
@@ -241,41 +246,63 @@ int main()
             return voxmat
     */
 
+    // ------------------- Voxellate ------------------- //
+
     // Put this in a function?
     //def voxellate(Nx, Ny, Nz, x_centres, y_centres, z_centres, posvecs):
-    vector<vector<vector<double>>> voxelmat(Nx,vector<vector<double>>(Ny,vector <double>Nz));
+    vector<vector<vector<double>>> voxelmat = vector<vector<vector<double> > > (Nx,vector<vector<double>>(Ny,vector <double>(Nz)));
+    //vector<vector<vector<double>>> voxelmat(Nx,vector<vector<double>>(Ny,vector <double>Nz));
+    // vector<vector<vector<int> > > v = vector<vector<vector<int> > >( 5, vector<vector<int> >(3, vector<int>(2, 4)))
     //vector<vector<vector<double>>> voxelmat; // = vector<vector<vector<double>>>(Nx,vector<vector<double>>(Ny,vector <double>Nz));
+
+    /*
+    char *filenamez = new char[1000];                                // File name can have max 1000 characters
+    sprintf(filenamez, "%s_spincorrelationfunctionz.txt", filenamePrefix.c_str() );   // Create filename with prefix and ending
+    spcorFilez.open(filenamez);
+    delete filenamez;
+    */
+
     int frame;
     ofstream outFile;
-    for(int i=0; i<Nout; i++){
-        frame = outframes[i];
-        voxelmat = voxellate_basic(frame, Nx, Ny, Nz, x_centres, y_centres, z_centres, xes, ys, zs);
+    for(int l=0; l<Nout; l++){
+        frame = outframes[l];
+        cout << "l = " << l << ", frame " << frame << endl;
+        voxelmat = voxellate_basic(l, Nall, Nx, Ny, Nz, x_centres, y_centres, z_centres, xes, ys, zs);
+
+        //cout << "Voxellation complete" << endl;
 
         // File name
-        char *ofilename = new char[1000]; // Possibly a long file name
-        outfilename = sprintf(ofilename, outfilename, frame); // Will this work?
+        char *ofilename = new char[100000]; // Possibly a long file name
+        sprintf(ofilename, "/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Fast_voxellation/chaingrid_quadratic_M9N101_ljdebye1.042_angle_Langevin_wall1.042_Kangle25_Kbond2000_T310_theta0is180_cutoffs3_sigma1_firstatomfixed_step%i", frame);
+        //ofilename = sprintf(ofilename, "%s.txt", outfilenamePrefix.c_str());
         outFile.open(ofilename);
         delete ofilename;
         // Write data to file
 
+        //cout << "Writing voxelmat to file" << endl;
         for(int i=0; i<Nx; i++){
+            //cout << "l = " << l << ", frame: " << frame << ": i = " << i << " of " << Nx << endl;
             for(int j=0; j<Ny; j++){
                 for(int k=0; k<Nz; k++){
                     // Write to file
-                    outFile << std::setprecision(std::numeric_limits<double>::digits10+1) << voxmat[i][j][k] << " ";
+                    outFile << std::setprecision(std::numeric_limits<double>::digits10+1) << voxelmat[i][j][k] << " ";
                 } // Close loop over z
                 outFile << endl;
             } // Close loop over y
         } // Close loop over x
-
         outFile.close();
+        //cout << "Closing file" << endl;
+        //cout << "outframes[0]: " << outframes[0] << ", outframes[1]: " << outframes[1] << ", outframes[2]: " << outframes[2] << endl;
+    } // Close loop over frames
 
-    }
+
+    //cout << "Done with writing voxelmat to file" << endl;
 
     ofstream dataFile;
-    //char *dfilename = new char[1000]; // Possibly a long file name
-    dataFile.open(datafilename); // Does this work?
-    delete datafilename;
+    char *dfilename = new char[100000]; // Possibly a long file name
+    sprintf(dfilename, "%s_data.txt", outfilenamePrefix.c_str());
+    dataFile.open(dfilename); // Does this work?
+    delete dfilename;
     dataFile << "len_voxel: " << lenvoxel << endl;
     dataFile << "Nx: " << Nx << " Ny: " << Ny << " Nz: " << Nz << endl;
     dataFile << "maxx: " << maxx << " minx: " << minx << endl;
@@ -283,51 +310,95 @@ int main()
     dataFile << "maxz: " << maxz << " minz: " << minz << endl;
     dataFile.close();
 
+    // Voxel centre coordinates
     ofstream xFile;
+    char *xfilename = new char[100000]; // Possibly a long file name
+    sprintf(xfilename, "%s_x_centres.txt", outfilenamePrefix.c_str());
     xFile.open(xfilename);
     delete xfilename;
 
     ofstream yFile;
+    char *yfilename = new char[100000]; // Possibly a long file name
+    sprintf(yfilename, "%s_y_centres.txt", outfilenamePrefix.c_str());
     yFile.open(yfilename);
     delete yfilename;
 
     ofstream zFile;
+    char *zfilename = new char[100000]; // Possibly a long file name
+    sprintf(zfilename, "%s_z_centres.txt", outfilenamePrefix.c_str());
     zFile.open(zfilename);
     delete zfilename;
+
+    // Voxel indices
+    ofstream xindFile;
+    char *xindfilename = new char[100000]; // Possibly a long file name
+    sprintf(xindfilename, "%s_xind.txt", outfilenamePrefix.c_str());
+    xindFile.open(xindfilename);
+    delete xindfilename;
+
+    ofstream yindFile;
+    char *yindfilename = new char[100000]; // Possibly a long file name
+    sprintf(yindfilename, "%s_yind.txt", outfilenamePrefix.c_str());
+    yindFile.open(yindfilename);
+    delete yindfilename;
+
+    ofstream zindFile;
+    char *zindfilename = new char[100000]; // Possibly a long file name
+    sprintf(zindfilename, "%s_zind.txt", outfilenamePrefix.c_str());
+    zindFile.open(zindfilename);
+    delete zindfilename;
 
     for(int i=0; i<Nx; i++){
         for(int j=0; j<Ny; j++){
             for(int k=0; k<Nz; k++){
                 // Write to file
+                // Coordinates
                 xFile << std::setprecision(std::numeric_limits<double>::digits10+1) << x_centres[i] << " "; // Is this really necessary?
                 yFile << std::setprecision(std::numeric_limits<double>::digits10+1) << y_centres[j] << " ";
                 zFile << std::setprecision(std::numeric_limits<double>::digits10+1) << z_centres[k] << " ";
+                // Indices
+                xindFile << i << " "; // Is this really necessary?
+                yindFile << j << " ";
+                zindFile << k << " ";
+                //if(i==1 && j==1 && k==1){
+                //    cout << "x_centres[i]:" <<  x_centres[i] << "; y_centres[j]:" <<  y_centres[i] << "; z_centres[k]:" <<  z_centres[i] << endl;
+                //}
             } // Close loop over z
+            // End lines, coordinate files
             xFile << endl;
             yFile << endl;
             zFile << endl;
+            // End lines, index files
+            xindFile << endl;
+            yindFile << endl;
+            zindFile << endl;
         } // Close loop over y
     } // Close loop over x
 
+    //cout << "Should have written to coordinates files now" << endl;
+    // Close coordinate files
     xFile.close();
     yFile.close();
     zFile.close();
+    // Close index files
+    xindFile.close();
+    yindFile.close();
+    zindFile.close();
 
     /*
     */
 
-    // ------------------- Voxellate ------------------- //
-
-
-    cout << "Hello" << ". " << "It is me" << "\n" << totframes << "\n" << indices[0] << "\n" << indices[1] << endl;
-
     /*// A formulation like this would be handier
     char filename2[900];
-    filename =
     sprintf(filename2, 'chaingrid_quadratic_M%iN%i_gridspacing%i_Langevin_wall%.3f_Kangle%.13f_Kbond%.12f_debye_kappa1_debyecutoff3_charge%i_T%i_theta0is180_twofirst_are_fixed.lammpstrj', M, N, gridspacing, wall, Kangle, Kbond, charge, T);
     */
 
-    cout << filename << endl;
+    //cout << "outframes[0]: " << outframes[0] << ", outframes[1]: " << outframes[1] << ", outframes[2]: " << outframes[2] << endl;
+
+    for(int i=0; i<Nout; i++){cout << "outframes[" << i << "]:" << outframes[i] << " ";}
+    cout << endl;
+
+    cout << "File done" << endl;
 
     return 0;
 }
@@ -345,33 +416,51 @@ vector<int> int_linspace(int framefirst, int framelast, int Nframes) // This doe
     return linspace_array;
 }
 
-vector<vector<vector<double>>> voxellate_basic(int frame, int Nx, int Ny, int Nz, vector<double> x_centres, vector<double> y_centres, vector<double> z_centres, vector<vector<double>> xes, vector<vector<double>> ys, vector<vector<double>> zs)
+vector<vector<vector<double>>> voxellate_basic(int frame, int Nall, int Nx, int Ny, int Nz, vector<double> x_centres, vector<double> y_centres, vector<double> z_centres, vector<vector<double>> xes, vector<vector<double>> ys, vector<vector<double>> zs)
 {
+    //cout << "In voxellate_basic" << endl;
     double dx, dy, dz, xc, yc, zc, xthis, ythis, zthis, smallest_dist, dotprod;
-    vector<vector<vector<double>>> voxmat(Nx,vector<vector<double>>(Ny,vector <double>Nz));
+    //cout << "Variables declared" << endl;
+    vector<vector<vector<double>>> voxmat = vector<vector<vector<double>>> (Nx,vector<vector<double>>(Ny,vector <double>(Nz)));
+    //cout << "Matrix initiated" << endl;
+    // vector<vector<vector<int> > > v = vector<vector<vector<int> > >( 5, vector<vector<int> >(3, vector<int>(2, 4)))
     // I'm gonna loop over the beads instead (there are fewer of them)
     for(int i=0; i<Nx; i++){
+        //cout << "In loop in voxellate_basic, i = " << i << endl;
         xc = x_centres[i];
+        //cout << "x_centres[" << i << "] extracted" << endl;
         for(int j=0; j<Ny; j++){
-            yc = y_centres[i];
+            //cout << "In loop in voxellate_basic, i = " << i << ", j = " << j << endl;
+            yc = y_centres[j];
+            //cout << "y_centres[" << j << "] extracted" << endl;
             for(int k=0; k<Nz; k++){
-                zc = z_centres[i];
+                //cout << "In loop in voxellate_basic, i = " << i << ", j = " << j << ", k = " << k << endl;
+                zc = z_centres[k];
+                //cout << "z_centres[" << k << "] extracted" << endl;
                 smallest_dist = 1e20; // No distance is this big
                 for(int m=0; m<Nall; m++){
+                    //cout << "In loop in voxellate_basic, i = " << i << ", j = " << j << ", k = " << k << ", m = " << m << endl;
                     xthis = xes[frame][m];
                     ythis = ys[frame][m];
                     zthis = zs[frame][m];
+                    //cout << "xthis etc. extracted" << endl;
                     dx = xthis-xc;
                     dy = ythis-yc;
                     dz = zthis-zc;
+                    //cout << "dx, etc. found" << endl;
                     dotprod = dx*dx +dy*dy +dz*dz;
+                    //cout << "dotprod calculated" << endl;
                     if(dotprod<smallest_dist){smallest_dist=dotprod;}
+                    //cout << "dotprod tested for smalles distance" << endl;
                 }
+                //cout << "Setting voxmat element" << endl;
                 voxmat[i][j][k] = sqrt(smallest_dist);
+                //cout << "voxmat element set" << endl;
             } // End loop over z-dir
         } // End loop over y-dir
     } // End loop over x-dir
 
+    //cout << "Loop done. Returning in a moment" << endl;
     return voxmat;
 }
 
