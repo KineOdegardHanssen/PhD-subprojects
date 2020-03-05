47f1f1e0193741a7512dc6eb750c628c53b9c953
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2020-03-05 15:35:39 +0100

    Added random walk
    Every site is allowed.
    
    So far, my system doesn't have periodic BCs. MIght change that. That would lead to changing the matrices a bit so that no two adjacent chains/boxes have distance 2d.

--- Walks_static_geometry/main.cpp
+++ Walks_static_geometry/main.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 // The preferred functions, I'd say: // Minor adjustment: intsigma gives the radius/half length of the structure, i.e. intsigma = 2 gives a sphere of diameter 5. Makes it easier to locate midpoint
+void randomwalk(int sigma, int d, int Nblocks, int Nrun, int Nrealizations, int Nsections);
 void matrixwalk_hard_easyinput(int sigma, int d, int Nblocks, int Nrun, int Nrealizations, int Nsections, int seed, bool printmatrix);
 void matrixmc_hard_easyinput(int sigma, int d, int Nblocks, int Nrun, int Nrealizations, int Nsections, int seed, bool printmatrix);
 void matrixmc_potential_easyinput(int intsigma, int intd, int Nblocks, int Nrun, int Nrealizations, int Nsections, int seed, double beta, double sigma, double power, double soften); // Power is expensive
@@ -47,7 +48,7 @@ int main()
 
     // Potential
     beta = 3.0;
-    sigma = 2.0;
+    sigma = 0.1;
     power = 6;
     soften = 1;
 
@@ -61,14 +62,192 @@ int main()
     //matrixmc_hard(Nblocks, blocksize, Nrun, Nrealizations, Nsections, seed);
     //matrixmc_potential(Nblocks, blocksize, Nrun, Nrealizations, Nsections, seed, beta, sigma, power, soften);
     //matrixwalk_hard_easyinput(intsigma, intd, Nblocks, Nrun, Nrealizations, Nsections, seed, printmatrix);
-    matrixmc_potential_easyinput(intsigma, intd, Nblocks, Nrun, Nrealizations, Nsections, seed, beta, sigma, power, soften); // Is there any
+    //matrixmc_potential_easyinput(intsigma, intd, Nblocks, Nrun, Nrealizations, Nsections, seed, beta, sigma, power, soften); // Is there any
     //matrixmc_hard_easyinput(intsigma, intd, Nblocks, Nrun, Nrealizations, Nsections, seed, printmatrix);
+    randomwalk(intsigma, intd, Nblocks, Nrun, Nrealizations, Nsections);
 
     cout << "Hello World!" << endl;
     return 0;
 }
 
 //---------------- Flexible functions ----------------//
+void randomwalk(int sigma, int d, int Nblocks, int Nrun, int Nrealizations, int Nsections)
+{   // Assuming quadratic matrix
+    // Sigma: 'radius' of obstactle (i.e. half the length since it is quadratic)
+    //---- Matrix set up ----//
+    int Nmat, blocksize;
+    blocksize = 2*sigma+1;
+    Nmat = (Nblocks+1)*d + Nblocks*blocksize; // Size of matrix: contribution from spacings + contributions from obstacles (Structure: sp-obst-sp-obst-...-sp-obst)
+
+    //---- Partitioning set up ----//
+    vector<int> startpoints = give_startpoints(Nrun, Nsections);
+    int len_sections = Nrun/Nsections; // I have already done this. Maybe having the function is not that handy...
+
+    //---- Set up of run ----//
+    // Random number distributions
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(0,Nmat-1);
+    std::uniform_int_distribution<int> steps_distr(0,1); // Move: +/-1. Should I be able to keep still too?
+    std::uniform_int_distribution<int> dir_distr(0,1);
+
+    // Variables
+    bool moveit;
+    //double thisR2;    // Is this a conflict? Should this be an int?
+    int starti, startj, indi, indj, nextindi, nextindj, dir, step, thisR2, nx, ny;
+    vector<double> walk_R2(Nrun);                                           // Average R^2
+    vector<double> walk_R2_rms(Nrun);                                       // RMS R^2
+    vector<vector<double>> walk_R2_store(Nrealizations, vector<double>(Nrun)); // For calculation of the standard deviation
+    vector<vector<double>> walk_x(Nrealizations, vector<double>(Nrun));        // Useful in case I want to look at the walks for different starting points (post-processing)
+    vector<vector<double>> walk_y(Nrealizations, vector<double>(Nrun));
+    vector<vector<double>> walk_R2_sections(Nsections, vector<double>(len_sections));                                                      // Sections
+    vector<vector<double>> walk_R2_sections_rms(Nsections, vector<double>(len_sections));                                                  // Sections, rms
+    vector<vector<vector<double>>> walk_R2_sections_store(Nrealizations, vector<vector<double>>(Nsections, vector<double>(len_sections))); // For calculation of the standard deviation
+
+
+    //---- Performing walk ----//
+    nx = 0; ny = 0;
+    for(int i=0; i<Nrealizations; i++){
+        // Start the walk by placing the bead in a random position
+        // and check that the position is not occupied
+        indi = distribution(generator); //location();//indi = rand() % Nmat; // Random numbers this way (from <random>) is a bit better than rand() from cstdlib, it seems.
+        indj = distribution(generator); //location();//indj = rand() % Nmat;
+        starti = indi;
+        startj = indj;
+        //cout << "i = " << i << ", starti = " << starti << ", startj = " << startj << endl;
+        for(int j=0; j<Nrun; j++){
+            moveit = false;
+            while(!moveit){    // Should I let the walker stand still if the move is not accepted?
+                step = 2*steps_distr(generator)-1;
+                //cout << "step: " << step << endl;
+                dir = dir_distr(generator);
+                //cout << "dir: " << dir << endl;
+                if(dir==0){
+                    //cout << "dir x chosen" << endl;
+                    nextindi = indi + step;
+                    nextindj = indj;
+                    if(nextindi<Nmat && nextindi>-1){
+                        moveit=true;
+                        nx++;
+                    }
+                }
+                else{
+                    //cout << "dir y chosen" << endl;
+                    nextindi = indi;
+                    nextindj = indj + step;
+                    if(nextindj<Nmat && nextindj>-1){ // If the site exists
+                        moveit=true;
+                        ny++;
+                    }
+                }
+            }
+            // Move the walker
+            indi = nextindi;
+            indj = nextindj;
+            // store data somewhere
+            // Average moves
+            thisR2 = (indi-starti)*(indi-starti) + (indj-startj)*(indj-startj);
+            walk_R2[j] += thisR2;
+            walk_R2_store[i][j] = thisR2;
+            // Saving coordinates
+            walk_x[i][j] = indi;
+            walk_y[i][j] = indj;
+        }
+        //cout << "indi: " << indi << ", indj: " << indj << endl;
+        //cout << "i = " << i << ", starti = " << starti << ", startj = " << startj << endl << "-------------------------------------" << endl;
+    }
+
+    for(int i=0; i<Nrun; i++){
+        walk_R2[i]/=Nrealizations;
+        //cout << "walk_R2[" << i << "]: " << walk_R2[i] << endl;
+    } // Averaging complete
+
+
+    // Finding rms values
+    for(int j=0; j<Nrun; j++){
+        for(int i=0; i<Nrealizations; i++){
+            walk_R2_rms[j] += (walk_R2_store[i][j]-walk_R2[j])*(walk_R2_store[i][j]-walk_R2[j]);
+        }
+        walk_R2_rms[j] = sqrt(walk_R2_rms[j]/(Nrealizations-1));
+    }
+
+    //cout << "nx: " << nx << endl << "ny: " << ny << endl;
+    //cout << "Nmat-1: " << Nmat-1 << endl;
+
+
+    //----- Partitioning -----//
+
+    int startpoint;
+    double startx, starty, currx, curry, dx, dy, dotprod;
+    for(int i=0; i<Nrealizations; i++){
+        for(int j=0; j<Nsections; j++){
+            // Extract starting points
+            startpoint = startpoints[j];
+            startx = walk_x[i][startpoint];
+            starty = walk_y[i][startpoint];
+            for(int k=1; k<len_sections; k++){ // Start at 1 since first point in array will be 0 anyways
+                currx   = walk_x[i][startpoint+k];
+                curry   = walk_y[i][startpoint+k];
+                dx      = currx-startx;
+                dy      = curry-starty;
+                dotprod = dx*dx+dy*dy;
+                walk_R2_sections[j][k]          += dotprod;
+                walk_R2_sections_store[i][j][k]  = dotprod;
+            } // End loop over steps (len_sections)
+        } // End loop over sections
+    } // End loop over realizations
+
+   // Averaging
+    for(int i = 0; i<Nsections; i++){
+        for(int j=0; j<len_sections; j++){
+            walk_R2_sections[i][j] /= Nrealizations;
+        } // End loop over steps (len_sections)
+    } // End loop over sections
+
+    // rms
+    for(int i=0; i<Nsections; i++){
+        for(int j=0; j<len_sections; j++){
+            for(int k=0; k<Nrealizations; k++){
+                walk_R2_sections_rms[i][j] = (walk_R2_sections_store[k][i][j]-walk_R2_sections[i][j])*(walk_R2_sections_store[k][i][j]-walk_R2_sections[i][j]);
+            } // End loop over realizations
+            walk_R2_sections_rms[i][j] = sqrt(walk_R2_sections_rms[i][j]/(Nrealizations-1));
+        } // End loop over steps (len_sections)
+    } // End loop over sections
+
+
+    //----- Writing to file -----//
+    //string outfilenamePrefix;
+
+
+    ofstream outFile;
+    char *filename = new char[100000]; // Possibly a long file name
+    sprintf(filename, "Nsteps%i_Nreal%i_sigma%i_d%i_Nblocks%i_randomwalk_R2_basic_mc", Nrun, Nrealizations, sigma, d, Nblocks);
+    outFile.open(filename);
+    delete filename;
+
+
+    for(int i=0; i<Nrun; i++){
+        outFile << i+1 << " " << walk_R2[i] << " " << walk_R2_rms[i] << endl;
+        //outFile << std::setprecision(std::numeric_limits<double>::digits10+1) << i+1 << " " << walk_R2[i] << " " << walk_R2_rms[i] << endl; // Is this long printing really neccessary? // Possibly (probably?) gonna use this later.
+    }
+    outFile.close();
+
+    ofstream poutFile;
+    char *pfilename = new char[100000]; // Possibly a long file name
+    sprintf(pfilename, "Nsteps%i_Nreal%i_sigma%i_d%i_Nblocks%i_Npart%i_randomwalk_R2_basic_mc", Nrun, Nrealizations, sigma, d, Nblocks, Nsections);
+    poutFile.open(pfilename);
+    delete pfilename;
+
+    for(int i=0; i<Nsections; i++){
+        poutFile << "--- Section " << i <<" ---" << endl;
+        for(int j=0; j<len_sections; j++){
+            poutFile << j << " " << walk_R2_sections[i][j] << " " << walk_R2_sections_rms[i][j] << endl;
+        } // End loop over steps (len_sections)
+    } // End loop over sections
+    poutFile.close();
+}
+
+
+
 void matrixwalk_hard_easyinput(int sigma, int d, int Nblocks, int Nrun, int Nrealizations, int Nsections, int seed, bool printmatrix)
 {   // Assuming quadratic matrix
     // Sigma: 'radius' of obstactle (i.e. half the length since it is quadratic)
