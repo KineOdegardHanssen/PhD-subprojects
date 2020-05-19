6b0c18e9e4a90b46156b9378489a47c1cd56d0ff
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2020-05-19 12:40:11 +0200

    Script to analyze exittimes
    So far: Making histograms (regular and cumulative). Not sure if I get that much intuition from that. Possible that I need to add more functionality. Maybe it helps to compare results for different d.

---
+++ MD_analysis/analyze_exittimes_brush.py
@@ -0,0 +1,142 @@
+from pylab import *
+import matplotlib.pyplot as plt                     # To plot
+import numpy as np
+import math
+
+damp = 10
+# Input parameters for file selection: # I will probably add more, but I want to make sure the program is running first
+spacing = 10
+psigma  = 1
+print('spacing:', spacing)
+print('psigma:', psigma)
+# I need to set the file name in an easier way, but for now I just use this:  ## Might want to add a loop too, if I have more files...
+
+# Should divide into folders in a more thorough manner?
+# Extracting the correct names (all of them)
+T            = 3
+confignrs    = np.arange(1,1001)#300)#20)#101)#1001)#22) 
+Nseeds       = len(confignrs)      # So that I don't have to change that much
+Nsteps       = 2001 # 20001 before
+unitlength   = 1e-9
+unittime     = 2.38e-11 # s
+timestepsize = 0.00045*unittime#*writeevery
+Npartitions  = 5 # For extracting more walks from one file (but is it really such a random walk here...?)
+minlength    = int(floor(Nsteps/Npartitions)) # For sectioning the data
+print('timestepsize:', timestepsize)
+
+endlocation          = '/home/kine/Projects_PhD/P2_PolymerMD/Planar_brush/Diffusion_bead_near_grid/Spacing%i/damp%i_diffseedLgv/Brush/Sigma_bead_' % (spacing,damp)+str(psigma) + '/'
+filestext            = 'config'+str(confignrs[0])+'to'+str(confignrs[-1])
+
+# Text files
+infilename_ds       = endlocation+'av_ds_'+filestext+'.txt'                        #'lammpsdiffusion_qdrgr_'+namebase+filestext+'_av_ds.txt'
+infilename_cuts     = endlocation+'cuts.txt'
+
+# Plots
+plotname             = endlocation+'par_ort_beginning_'+filestext+'_wcuttimes.png'                                 #'lammpsdiffusion_qdrgr_'+namebase+filestext+'.png'
+
+infile_ds = open(infilename_ds, 'r')
+firstline = infile_ds.readline()
+
+#Time step; Time; <R^2>; <dx^2>; <dy^2>; <dz^2>; <dx^2+dy^2>
+#  [0]       [1]   [2]     [3]    [4]      [5]       [6]
+timesteps = []
+times     = []
+dR2       = []
+dx2       = []
+dy2       = []
+dz2       = []
+dpar2     = []
+
+lines = infile_ds.readlines()
+
+for line in lines:
+    words = line.split()
+    timesteps.append(int(words[0]))
+    times.append(float(words[1]))
+    dR2.append(float(words[2]))
+    dx2.append(float(words[3]))
+    dy2.append(float(words[4]))
+    dz2.append(float(words[5]))
+    dpar2.append(float(words[6]))
+infile_ds.close()
+
+timesteps = np.array(timesteps)
+times     = np.array(times)
+dR2       = np.array(dR2)
+dx2       = np.array(dx2)
+dy2       = np.array(dy2)
+dz2       = np.array(dz2)
+dpar2     = np.array(dpar2)
+
+#
+infile_cuts = open(infilename_cuts, 'r')
+firstline   = infile_cuts.readline()
+
+#Time step; Time; <R^2>; <dx^2>; <dy^2>; <dz^2>; <dx^2+dy^2>
+#  [0]       [1]   [2]     [3]    [4]      [5]       [6]
+configs  = []
+cuttimes = []
+
+lines = infile_cuts.readlines()
+
+for line in lines:
+    words = line.split()
+    configs.append(int(words[0]))
+    cuttimes.append(float(words[1]))
+infile_cuts.close()
+
+configs  = np.array(configs)  # Do I even need the configs?
+cuttimes = np.array(cuttimes)
+
+
+## To determine the range
+# Interactive
+plt.figure(figsize=(6,5))
+plt.plot(timesteps, dR2, label=r'$<R^2>$')
+plt.plot(timesteps, dx2, label=r'$<dx^2>$')
+plt.plot(timesteps, dy2, label=r'$<dy^2>$')
+plt.plot(timesteps, dz2, label=r'$<dz^2>$')
+plt.plot(timesteps, dpar2, label=r'$<dx^2+dy^2>$')
+plt.xlabel(r'Index (s)')
+plt.ylabel(r'Distance$^2$ [in unit length]')
+plt.title(r'RMSD in brush, d = %i nm, $\sigma_b=%.2f$' % (spacing,psigma))
+plt.tight_layout()
+plt.legend(loc='upper left')
+#plt.show()
+
+# Saving
+'''
+xmax_plot = 100
+plt.figure(figsize=(6,5))
+plt.plot(timesteps, dR2, label=r'$<R^2>$')
+plt.plot(timesteps, dx2, label=r'$<dx^2>$')
+plt.plot(timesteps, dy2, label=r'$<dy^2>$')
+plt.plot(timesteps, dz2, label=r'$<dz^2>$')
+plt.plot(timesteps, dpar2, label=r'$<dx^2+dy^2>$')
+plt.xlabel(r'Index (s)')
+plt.ylabel(r'Distance$^2$ [in unit length]')
+plt.title(r'RMSD in brush, d = %i nm, $\sigma_b=%.2f$' % (spacing,psigma))
+plt.tight_layout()
+plt.legend(loc='upper left')
+plt.axis([0, xmax_plot, 0, max(dR2[0:xmax_plot])])
+plt.savefig(plotname)
+'''
+
+plt.figure(figsize=(6,5))
+plt.hist(cuttimes, bins=100)
+plt.xlabel(r'Exit time (Time of cut)')
+plt.ylabel(r'Number of exits')
+plt.title(r'Histogram of exit times, d = %i nm, $\sigma_b=%.2f$' % (spacing,psigma))
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(6,5))
+plt.hist(cuttimes, bins=100, cumulative=True)
+plt.xlabel(r'Exit time (Time of cut)')
+plt.ylabel(r'Number of exits')
+plt.title(r'Cumulative histogram of exit times, d = %i nm, $\sigma_b=%.2f$' % (spacing,psigma))
+plt.tight_layout()
+plt.show()
+
+
+
