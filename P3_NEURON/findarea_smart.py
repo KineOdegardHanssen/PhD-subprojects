3ff0340ef7e335219e1c01aaa5059cec76ce0f24
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2021-04-08 18:07:23 +0200

    Upload before drastic change
    About to implement on-the-fly calculations of area.

---
+++ P3_NEURON/findarea_smart.py
@@ -0,0 +1,190 @@
+import os
+from os.path import join
+import sys
+import matplotlib
+matplotlib.use("AGG")
+import matplotlib.pyplot as plt
+import json
+import neuron
+import numpy as np
+import math
+
+import LFPy
+
+## Choose model(s) to test:
+#testmodel = 515175347 #Axonabundance (Hundreds of axon sections, branched dends, at least one part of the neuron is not connected to the rest) # AXON CONNECTED TO DENDRITE!!!
+#testmodel = 501282059 #Originalplayer (Not everything is connected to the rest. 156 axon sec., 35 dend. sec.) # AXON CONNECTED TO DENDRITE!!!
+#testmodel = 496497595 #Developmentcell (One short axon, branched dendrites, everything seems to be connected, no errors)
+#testmodel = 497232392
+#testmodel = 496538958
+testmodel = 488462965 # PERISOMATIC model of Developmentcell
+#testmodel = 478513407 # Perisomatic version of 497233271, but only yields one peak
+#testmodel = 480633479 # Perisomatic version of 497230463, only one peak ####
+#testmodel = 478513437 # Perisomatic version of 497233075
+
+testmodelname = 'neur_%i' % testmodel
+all_models    = [testmodelname]
+
+if testmodel==480633479:
+    v_init = -96.8#-83.7#-90#-86.5# # Have a test here too
+elif testmodel==496497595:
+    v_init = -86.5
+elif testmodel==488462965:
+    v_init = -86.5 # Maybe I should have changed this...
+elif testmodel==497230463:
+    v_init = -90
+elif testmodel==497233075:
+    v_init = -90
+elif testmodel==478513437:
+    v_init = -86.8
+elif testmodel==478513407:
+    v_init = -83.7
+elif testmodel==497233271:
+    v_init = -90
+
+# Defaulting to original values:
+# DO NOT TOUCH THESE!
+# SET THEM BELOW INSTEAD!
+if testmodel==496497595:
+    cm_soma = 1.14805
+    cm_dend = 9.98231
+    cm_axon = 3.00603
+elif testmodel==497233271:
+    cm_soma = 0.783229
+    cm_dend = 1.94512
+    cm_axon = 8.25387
+elif testmodel==497230463:
+    cm_soma = 1.23729
+    cm_dend = 2.57923
+    cm_axon = 5.02697
+elif testmodel==497233075:
+    cm_soma = 1.64168
+    cm_dend = 2.83035
+    cm_axon = 9.98442
+elif testmodel==488462965:
+    cm_soma = 3.31732779736 # Strange values, right?
+    cm_dend = 3.31732779736
+    cm_axon = 3.31732779736
+elif testmodel==478513407:
+    cm_soma = 1.0
+    cm_dend = 1.0
+    cm_axon = 1.0
+elif testmodel==480633479:
+    cm_soma = 0.704866 # 0.704866118957
+    cm_dend = 0.704866 # 0.704866118957
+    cm_axon = 0.704866 # 0.704866118957
+elif testmodel==478513437:
+    cm_soma = 2.34539964752
+    cm_dend = 2.34539964752
+    cm_axon = 2.34539964752
+
+
+
+def return_allen_cell_model(model_folder):
+
+    params = json.load(open(join(model_folder, "fit_parameters.json"), 'r'))
+
+    Ra = params["passive"][0]["ra"]
+
+    e_pas = params["passive"][0]["e_pas"]
+    celsius = params["conditions"][0]["celsius"]
+    #cms = params["passive"][0]["cm"]
+    reversal_potentials = params["conditions"][0]["erev"]
+    v_init = params["conditions"][0]["v_init"]
+    active_mechs = params["genome"]
+    neuron.h.celsius = celsius
+    # print(Ra, celsius, v_init)
+    # print(reversal_potentials)
+    # print(active_mechs)
+
+
+    # Define cell parameters
+    cell_parameters = {
+        'morphology': join(model_folder, 'reconstruction.swc'),
+        'v_init': -93.3,    # initial membrane potential
+        'passive': False,   # turn on NEURONs passive mechanism for all sections
+        'nsegs_method': 'fixed_length', # spatial discretization method
+        'max_nsegs_length': 20.,
+        #'lambda_f' : 200.,           # frequency where length constants are computed
+        'dt': 2.**-7,      # simulation time step size
+        'tstart': -100.,      # start time of simulation, recorders start at t=0
+        'tstop': 100.,     # stop simulation at 100 ms.
+        'custom_code': ['remove_axon.hoc']
+    }
+
+    cell = LFPy.Cell(**cell_parameters)
+    cell.set_rotation(z=np.pi/1.25)
+
+    Ndendsecs=0
+    for sec in neuron.h.allsec():
+        sec.insert("pas")
+        sec.e_pas = e_pas
+        sec.Ra = Ra
+        sectype = sec.name().split("[")[0]
+        if sectype=="dend":
+            Ndendsecs+=1
+        
+        for sec_dict in active_mechs:
+            if sec_dict["section"] == sectype:
+                if sectype=="soma": # Works
+                    exec("sec.cm = {}".format(cm_soma))
+                if sectype=="dend": # Works
+                    exec("sec.cm = {}".format(cm_dend))
+                if sectype=="axon": # Works
+                    exec("sec.cm = {}".format(cm_axon))
+                if not sec_dict["mechanism"] == "":
+                    sec.insert(sec_dict["mechanism"])
+                exec("sec.{} = {}".format(sec_dict["name"], sec_dict["value"]))
+        
+        for sec_dict in reversal_potentials:
+            if sec_dict["section"] == sectype:
+                # print(sectype, sec_dict)
+                for key in sec_dict.keys():
+                    if not key == "section":
+                        exec("sec.{} = {}".format(key, sec_dict[key]))
+    return cell, Ndendsecs
+
+cell_models_folder = "cell_models" #"cell_models_Arkhipov_et_al" #"all_cell_models"
+
+#all_models = [f for f in os.listdir(cell_models_folder) if f.startswith("neur")
+#              and os.path.isdir(join(cell_models_folder, f))]
+mod_folder = "Allen_test_changecapacitance/cell_models/"+testmodelname+"/modfiles"
+if "win64" in sys.platform:
+    print('Detected sys.platform as win64')
+    warn("no autompile of NMODL (.mod) files on Windows. " 
+         + "Run mknrndll from NEURON bash in the folder cells and rerun example script")
+    if not pth in neuron.nrn_dll_loaded:
+        neuron.h.nrn_load_dll(mod_folder+"/nrnmech.dll")
+    neuron.nrn_dll_loaded.append(mod_folder)
+
+for model_idx in range(len(all_models)):
+    model_name = all_models[model_idx]
+    #print(model_idx, model_name)
+    model_folder = join("cell_models", model_name)
+
+    cell, Ndendsecs = return_allen_cell_model(model_folder)
+    print('Ndendsecs:',Ndendsecs)
+    
+    # print out section information: # Works even though I do everything through LFPy
+    diams  = []
+    lengths= []
+    for sec in neuron.h.allsec():
+        neuron.h.psection()
+        lengths.append(sec.L)
+        j=0
+        for seg in sec:
+            if j==0:
+                diams.append(seg.diam)
+                j=1
+            print('seg:', seg, '; diam:', seg.diam)
+    
+    print('len(lengths):',len(lengths))
+    print('len(diams):',len(diams))
+    area = 0
+    N = len(diams)
+    for i in range(N):
+        area+=lengths[i]*diams[i]
+    area*=np.pi
+    
+    print('Cell model:', testmodel)
+    print('Area:',area)
