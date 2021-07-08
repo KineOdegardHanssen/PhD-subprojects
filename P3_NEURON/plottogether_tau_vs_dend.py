2b3a7d021d10bb4e75c3d24603ee2005c6eeb2eb
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2021-07-08 16:19:32 +0200

    Generalized plotting script
    That was fast.

--- P3_NEURON/plottogether_tau_vs_dend.py
+++ P3_NEURON/plottogether_tau_vs_dend.py
@@ -2,15 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# Later on: Maybe include option to vary the dendrite length too. Possibly do that in another script.
+varydiam = False
 
-zoomed = False
+zoomed = True
 somasize = 10
 dendlen  = 10
-denddiams = [0,1,2,5,10,20]
+denddiam = 5
+if varydiam==True:
+    denddiams = [0,1,2,5,10,20]
+    N = len(denddiams)
+else:
+    dendlens = [0,1,2,5,10,20,50,100]
+    N = len(dendlens)
+    
 Ra     = 100
 v_init = -70
-N = len(denddiams)
 
 idur = 1000   # ms
 iamp = -0.1   # nA 
@@ -21,14 +27,23 @@ somaonly_folder = 'Somaonly/pas/Results/IStim/Soma10/'+currentfolder
 filename_nodend = somaonly_folder+'somaonly_cms_idur%i_iamp'%idur+str(iamp)+'_Ra'+str(Ra)+'_vinit'+str(v_init)+'_pas_tau_highCms.txt'
 filenames.append(filename_nodend)
 plotfolder = 'Comparemodels/BAS_vs_somaonly_passive/'
-plotname = plotfolder + 'BAS_vs_onecomp_cms_idur%i_iamp'%idur+str(iamp)+'_Ra'+str(Ra)+'_vinit'+str(v_init)+'_pas_tau'
+
+if varydiam==True:
+    endsnippet = '_varydenddiam'
+else:
+    endsnippet = '_varydendlen'
+
+plotname = plotfolder + 'BAS_vs_onecomp_cms_idur%i_iamp'%idur+str(iamp)+'_Ra'+str(Ra)+'_vinit'+str(v_init)+'_pas_tau'+endsnippet
 if zoomed==True:
     plotname=plotname+'_zoomed.png'
 else:
     plotname=plotname+'.png'
 
 for i in range(1,N):
-    denddiam = denddiams[i]
+    if varydiam==True:
+        denddiam = denddiams[i]
+    else:
+        dendlen = dendlens[i]
     folder = 'Ball-and-stick models/BAS_passive/Results/IStim/Soma%i/dendlen%i/denddiam%i/' % (somasize,dendlen,denddiam)+currentfolder
     filename = folder + 'baspass_cms_idur%i_iamp' % idur+str(iamp)+'_Ra'+str(Ra)+'_vinit'+str(v_init)+'_pas_tau_highCms.txt'
     filenames.append(filename)
@@ -46,9 +61,15 @@ for i in range(N):
             cms.append(float(words[0]))
             taus.append(float(words[1]))
     if zoomed==False:
-        plt.plot(cms,taus,label='Dendrite diam: %i' % denddiams[i])
+        if varydiam==True:
+            plt.plot(cms,taus,label='Dendrite diam: %i' % denddiams[i])
+        else:
+            plt.plot(cms,taus,label='Dendrite len: %i' % dendlens[i])
     else:
-        plt.plot(cms[2:7],taus[2:7],label='Dendrite diam: %i' % denddiams[i])
+        if varydiam==True:
+            plt.plot(cms[2:7],taus[2:7],label='Dendrite diam: %i' % denddiams[i])
+        else:
+            plt.plot(cms[2:7],taus[2:7],label='Dendrite len: %i' % dendlens[i])
     file.close()
 plt.xlabel(r'$C_m$ [$\mu$F/cm$^2$] (Cell parameter)')
 plt.ylabel(r'$\tau_m$ [ms] (Measured value)')
